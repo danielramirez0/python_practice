8231a773b4a1f47b13d5ebe14f8578b80a5b9ee9
Daniel Ramirez <zoerunner@gmail.com>
2021-10-13 19:39:20 -0700

    Adds variables for pyenv use

---
+++ Intermediate/Turtle_Graphics/main.py
@@ -0,0 +1,17 @@
+#####Turtle Intro######
+
+import turtle as t
+
+timmy_the_turtle = t.Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("red")
+
+######## Challenge 1 - Draw a Square ############
+
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)

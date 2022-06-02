37d440a865cb5231411a43255d5c0b643f3a785f
Daniel Ramirez <zoerunner@gmail.com>
2022-06-02 13:21:03 -0700

    Adds gitingore and turtle graphics files

--- Intermediate/Turtle_Graphics/main.py
+++ Intermediate/Turtle_Graphics/main.py
@@ -1,10 +1,18 @@
 #####Turtle Intro######
-
-import turtle as t
-
-timmy_the_turtle = t.Turtle()
+from turtle import Turtle as t, Screen as s
+timmy_the_turtle = t()
 timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("red")
+# color('red', 'yellow')
+# begin_fill()
+# while True:
+#     forward(200)
+#     left(170)
+#     if abs(pos()) < 1 :
+#         break
+# end_fill()
+
+
 
 ######## Challenge 1 - Draw a Square ############
 
@@ -15,3 +23,6 @@ timmy_the_turtle.right(90)
 timmy_the_turtle.forward(100)
 timmy_the_turtle.right(90)
 timmy_the_turtle.forward(100)
+
+screen = s()
+screen.exitonclick()

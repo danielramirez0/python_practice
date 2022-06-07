626fa79761ebe26559c30335cf86bbf66c109f5f
Daniel Ramirez <zoerunner@gmail.com>
2022-06-06 17:11:20 -0700

    Add frogger game

--- Intermediate/Turtle_Crossing/player.py
+++ Intermediate/Turtle_Crossing/player.py
@@ -1,7 +1,25 @@
+from turtle import Turtle
+
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    
+    def __init__(self, shape) -> None:
+        super().__init__(shape)
+        self.setheading(90)
+        self.penup()
+        self.goto(STARTING_POSITION)
+
+    def move(self):
+        self.forward(MOVE_DISTANCE)
+
+    def crossed_finish(self):
+        if self.ycor() > FINISH_LINE_Y:
+            self.goto(STARTING_POSITION)
+            return True
+        else:
+            return False
--- Intermediate/Turtle_Crossing/scoreboard.py
+++ Intermediate/Turtle_Crossing/scoreboard.py
@@ -1,5 +1,33 @@
+from turtle import Turtle
+
+FONT_ALIGNMENT = "center"
+FONT_NAME = "Arial"
+FONT_COLOR = "black"
 FONT = ("Courier", 24, "normal")
 
+class Scoreboard(Turtle):
 
-class Scoreboard:
-    pass
+    def __init__(self) -> None:
+        super().__init__()
+        self.level = 1
+        self.hideturtle()
+        self.penup()
+        self.color(FONT_COLOR)
+        self.goto(-220, 260)
+        self.update_score()
+    
+    def update_score(self):
+        self.write(f"Level: {self.level}", align=FONT_ALIGNMENT, font=(FONT))
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write(f"GAME OVER", align=FONT_ALIGNMENT, font=(FONT))
+
+    def increase_score(self):
+        self.clear()
+        self.level += 1
+        self.update_score()
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write("GAME OVER", align=FONT_ALIGNMENT, font=(FONT))

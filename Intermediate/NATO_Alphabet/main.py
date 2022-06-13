67db286f0503ab02d7afb0f94dda09112c77abe2
Daniel Ramirez <zoerunner@gmail.com>
2022-06-13 10:28:01 -0700

    Updates NATO phonetic with error handling

--- Intermediate/NATO_Alphabet/main.py
+++ Intermediate/NATO_Alphabet/main.py
@@ -3,8 +3,14 @@ data = pandas.read_csv("nato_phonetics.csv")
 
 phonetics = {row.letter:row.code for (index, row) in data.iterrows()}
 
-while True:
-    word = input("Type a word or (q) to exit: ").upper()
-    if word == "Q": break
-    print([phonetics[letter] for letter in word])
+def generate_phonetic():
+    word = input("Type a word: ").upper()
+    try:
+        phonetic_list = [phonetics[letter] for letter in word]
+    except KeyError:
+        print("Only letters are allowed")
+        generate_phonetic()
+    else:
+        print(phonetic_list)
 
+generate_phonetic()

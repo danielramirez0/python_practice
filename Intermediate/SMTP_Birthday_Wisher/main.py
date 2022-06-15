4f724e37b2cbaca411c3b9d4e29627b20a8e46a8
Daniel Ramirez <zoerunner@gmail.com>
2022-06-15 08:47:38 -0700

    Shows another solution of card setup for email

--- Intermediate/SMTP_Birthday_Wisher/main.py
+++ Intermediate/SMTP_Birthday_Wisher/main.py
@@ -6,6 +6,7 @@ from libs.email_client import send_email
 
 DATA = pandas.read_csv("birthdays.csv")
 RECORDS = DATA.to_dict(orient="records")
+# RECORDS = {(row.month, row.day): row for (index, row) in DATA.iterrows()} # Angela's solution
 TEMPLATE_FILES = ["letter_1.txt", "letter_2.txt", "letter_3.txt"]
 TIME = dt.datetime.now()
 
@@ -15,5 +16,6 @@ for record in RECORDS:
     birthday = f'{record["month"]:02d}-{record["day"]:02d}'
     if todays_date == birthday:
         with open(f"letter_templates/{choice(TEMPLATE_FILES)}") as template:
-            card = "".join(line.replace("[NAME]", record["name"]) for line in template.readlines())
-        send_email("anela828@gmail.com", cred, "Happy Birthday", card)
+            # card = "".join(line.replace("[NAME]", record["name"]) for line in template.readlines())
+            card = template.read().replace("[NAME]", record["name"])
+        send_email(record["email"], cred, "Happy Birthday", card)

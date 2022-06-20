7e3c23a83f431f8e46fdb1f633910ce8c1079d8a
Daniel Ramirez <zoerunner@gmail.com>
2022-06-20 15:06:51 -0700

    Adds workout tracker using sheety API and nutritionix natural exercise API

---
+++ Intermediate/workout_tracker_Google_Sheets_NutritionAPI/main.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+import requests
+from env.config import GENDER, HEADERS, AGE, HEIGHT, WEIGHT, EXERCISE_ENDPOINT, SHEETS_ENDPOINT
+
+
+
+workout = input("What did you do at the gym? ")
+dt = datetime.now()
+date_str = dt.strftime("%m/%d/%Y")
+# time_str = dt.strftime("%H:%M:%S")
+time_str = dt.strftime("%X")
+
+body = {
+    "query": workout,
+    "gender": GENDER,
+    "weight_kg": WEIGHT,
+    "height_cm": HEIGHT,
+    "age": AGE
+}
+
+r = requests.post(url=EXERCISE_ENDPOINT, headers=HEADERS, json=body)
+r.raise_for_status()
+exercises = r.json()["exercises"]
+
+for exercise in exercises:
+    body = { 
+        "workout":{
+            "date": date_str,
+            "time": time_str,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+    r = requests.post(url=SHEETS_ENDPOINT, json=body)
+    r.raise_for_status()
+    print(r.text)
+
+
